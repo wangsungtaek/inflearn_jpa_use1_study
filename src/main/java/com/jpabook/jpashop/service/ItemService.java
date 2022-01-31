0fc7faff29c32403857aac3f31aff62777aaf37d
wangsungtaek <tjdxorv@naver.com>
2022-01-31 18:12:04 +0900

    상품 서비스 구현

---
+++ src/main/java/com/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,32 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.Repository.ItemRepository;
+import com.jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional(readOnly = false)
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+
+}
